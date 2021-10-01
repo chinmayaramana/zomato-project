48697499922b0b13df461bc1a42d6f4b409dbe5e
Chinmaya Ramana <82995419+Anonymous-Agent@users.noreply.github.com>
2021-10-02 02:57:47 +0530

    Added google sign in

--- server/index.js
+++ server/index.js
@@ -5,7 +5,13 @@ require("dotenv").config();
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import passport from "passport";
 
+// configs
+import googleAuthConfig from "./config/google.config";
+
+// microservice routes
+import Auth from "./API/Auth";
 
 
 // Database connection
@@ -18,6 +24,12 @@ zomato.use(express.json());
 zomato.use(express.urlencoded({ extended: false }));
 zomato.use(helmet());
 zomato.use(cors());
+
+// passport cofiguration
+googleAuthConfig(passport);
+
+// Application Routes
+zomato.use("/auth", Auth);
 
 zomato.get("/", (req, res) => res.json({ message: "Setup success" }));
 
